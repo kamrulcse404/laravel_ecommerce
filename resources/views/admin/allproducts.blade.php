772e42861cc95f337328bea96e34103dce7f7188
kamrulcse404 <kamrul.h01960@gmail.com>
2023-12-06 18:52:12 +0600

    product form and table setup

--- resources/views/admin/allproducts.blade.php
+++ resources/views/admin/allproducts.blade.php
@@ -3,5 +3,36 @@
     All Products - Single Ecom
 @endsection
 @section('content')
-    <h1>Hello All products</h1>
+    <div class="container-xxl flex-grow-1 container-p-y">
+        <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Page/</span> All Products</h4>
+        <div class="card">
+            <h5 class="card-header">Available Products Information</h5>
+            <div class="table-responsive text-nowrap">
+                <table class="table">
+                    <thead class="table-light">
+                        <tr>
+                            <th>Id</th>
+                            <th>Product Name</th>
+                            <th>Image</th>
+                            <th>Price</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-border-bottom-0">
+                        <tr>
+                            <td>1</td>
+                            <td>Fan</td>
+                            <td></td>
+                            <td>100</td>
+                            <td>
+                                <a href="" class="btn btn-primary">Edit</a>
+                                <a href="" class="btn btn-warning">Delete</a>
+                            </td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 @endsection
